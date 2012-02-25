501930338495853d750978c31dbc411b3d47a56d
Stefan Kanev <stefan.kanev@gmail.com>
2012-02-26 01:01:31 +0200

    Extract a helper for defining valid moves

--- spec/lib/chessboard/piece/rook_spec.rb
+++ spec/lib/chessboard/piece/rook_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 module Chessboard
   describe Rook do
     it "can move horizontally and vertically" do
-      board = parse_board <<-END
+      expect_allowed_moves <<-END
         ┌───┬───┬───┬───┬───┬───┬───┬───┐
         │   │   │   │   │( )│   │   │   │
         ├───┼───┼───┼───┼───┼───┼───┼───┤
@@ -23,6 +23,10 @@ module Chessboard
         │   │   │   │   │( )│   │   │   │
         └───┴───┴───┴───┴───┴───┴───┴───┘
       END
+    end
+
+    def expect_allowed_moves(drawing)
+      board = parse_board drawing
 
       board.marked_squares.each do |square|
         board.should be_allowing_move board.selected_square, square
